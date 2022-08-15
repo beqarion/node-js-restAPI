8851d1f2e3e7930572b4fd80ebc6b7fadd5f6c21
Bekarion <beqarioni@yahoo.co.uk>
2022-08-15 22:57:44 +0400

    first commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const mongoose = require("mongoose")
 
 const feedRoutes = require("./routes/feed")
 
@@ -16,4 +17,11 @@ app.use((req, res, next) => {
 
 app.use("/feed", feedRoutes)
 
-app.listen(8080)
+mongoose
+  .connect(
+    "mongodb+srv://beqarioni:sheyiladzee@cluster0.qmkq6.mongodb.net/messages?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    app.listen(8080)
+  })
+  .catch((err) => console.log(err))

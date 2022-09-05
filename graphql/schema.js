b63052522e53b9c8ca33f95b9c8224a825255b4b
beqarion <beqarioni@yahoo.co.uk>
2022-09-05 18:11:09 +0400

    work->home

--- graphql/schema.js
+++ graphql/schema.js
@@ -1,41 +1,46 @@
 const { buildSchema } = require("graphql")
 
 module.exports = buildSchema(`
-    type Post {
-        _id: ID!
-        title: String!
-        content: String!
-        imageUrl: String!
-        creator: User!
-        createdAt: String!
-        updatedAt: String!
-    }
+  type Post {
+    _id: ID!
+    title: String!
+    content: String!
+    imageUrl: String!
+    creator: User!
+    createdAt: String!
+    updatedAt: String!
+  }
 
-    type User {
-        _id: ID!
-        name: String!
-        email: String!
-        password: String
-        status: String!
-        posts: [Post!]!
-    }
-        
-    input UserInputData {
-        email: String!
-        name: String!
-        password: String!
-    }
+  type User {
+    _id: ID!
+    name: String!
+    email: String!
+    password: String
+    status: String!
+    posts: [Post!]!
+  }
 
-    type RootQuery {
-        hello: String
-    }
+  type AuthData {
+    token: String!
+    userId: String!
+  }
+      
+  input UserInputData {
+    email: String!
+    name: String!
+    password: String!
+  }
 
-    type RootMutation {
-        createUser(userInput: UserInputData): User!
-    }
+  type RootQuery {
+    login(email: String!, password: String!): AuthData!
+  }
 
-    schema {
-        query: RootQuery
-        mutation: RootMutation
-    }
+  type RootMutation {
+    createUser(userInput: UserInputData): User!
+  }
+
+  schema {
+    query: RootQuery
+    mutation: RootMutation
+  }
 `)

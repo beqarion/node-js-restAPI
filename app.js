7a77b19a8eb465f78d4ec2f605c6d38bf33c4dc8
beqarion <beqarioni@yahoo.co.uk>
2022-09-08 19:16:10 +0400

    work->home

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const { graphqlHTTP } = require("express-graphql")
 
 const graphqlSchema = require("./graphql/schema")
 const graphqlResolver = require("./graphql/resolvers")
+const auth = require("./middleware/auth")
 
 const app = express()
 
@@ -45,11 +46,13 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*")
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE")
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
-  if (req.method === 'OPTIONS'){
+  if (req.method === "OPTIONS") {
     return res.sendStatus(200)
   }
   next()
 })
+
+app.use(auth)
 
 app.use(
   "/graphql",
--- middleware/auth.js
+++ middleware/auth.js
@@ -3,17 +3,16 @@ const jwt = require("jsonwebtoken")
 module.exports = (req, res, next) => {
   const authHeader = req.get("Authorization")
   if (!authHeader) {
-    const error = new Error("Not authenticated.")
-    error.statusCode = 401
-    throw error
+    req.isAuth = false
+    return next()
   }
   const token = authHeader.split(" ")[1]
   let decodedToken
   try {
-    decodedToken = jwt.verify(token, "secret")
+    decodedToken = jwt.verify(token, "someSecret")
   } catch (err) {
-    err.statusCode = 500
-    throw err
+    req.isAuth = false
+    return next()
   }
   if (!decodedToken) {
     const error = new Error("Not authenticated.")
@@ -21,5 +20,6 @@ module.exports = (req, res, next) => {
     throw error
   }
   req.userId = decodedToken.userId
+  req.isAuth = true
   next()
 }
